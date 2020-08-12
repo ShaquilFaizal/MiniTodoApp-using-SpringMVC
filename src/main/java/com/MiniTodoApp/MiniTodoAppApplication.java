f83c7a591c3019ab5c3dddc32ba39f01de4b9f45
Shaquil Faizal <shaquil.faizal@gmail.com>
2020-08-12 20:42:10 +0530

    add Initlzr project

---
+++ src/main/java/com/MiniTodoApp/MiniTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.MiniTodoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniTodoAppApplication.class, args);
+	}
+
+}
