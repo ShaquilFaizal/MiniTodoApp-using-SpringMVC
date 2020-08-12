e728fcd4c17495a77379b13cc99312ff6edf6398
Shaquil Faizal <shaquil.faizal@gmail.com>
2020-08-12 21:31:43 +0530

    added Todo services implementation

--- src/main/java/com/MiniTodoApp/Repositories/TodoRepository.java
+++ src/main/java/com/MiniTodoApp/Repositories/TodoRepository.java
@@ -1,9 +1,14 @@
 package com.MiniTodoApp.Repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.MiniTodoApp.Model.Todo;
 
 public interface TodoRepository extends JpaRepository<Todo /*TodoModel*/, Long /*Id Datatype*/> {
-
+	
+	List<Todo> findByUsername(String user);
+	
+	
 }
