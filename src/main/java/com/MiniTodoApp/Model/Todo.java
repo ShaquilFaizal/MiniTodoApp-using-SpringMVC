b27b5e64b372fa4f744ed5577ce5337ae60fd6dd
Shaquil Faizal <shaquil.faizal@gmail.com>
2020-08-13 23:42:41 +0530

    updated todo model

--- src/main/java/com/MiniTodoApp/Model/Todo.java
+++ src/main/java/com/MiniTodoApp/Model/Todo.java
@@ -6,11 +6,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Table
 public class Todo {
 
 	@Id
